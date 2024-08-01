52cd9ace46a5ba0381bef631e81d08d464c7f286
teddyassih <teddyassih@gmail.com>
2024-08-01 17:11:57 +0000

    last commit

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
@@ -10,8 +11,11 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react"
 
 export default function LoginForm() {
+
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Card className="mx-auto max-w-sm">
